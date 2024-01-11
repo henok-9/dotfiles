30c126af09c2462f3dc6c765c635513051c76bf8
henok-9 <henokabe89@gmail.com>
2024-01-11 17:18:30 -0500

    chore: style update & general fixes

--- config/hypr/scripts/sleep.sh
+++ config/hypr/scripts/sleep.sh
@@ -1,4 +1,4 @@
-swayidle -w timeout 600 'swaylock -f -c  FFC300 ' \
-            timeout 1800 'hyprctl dispatch dpms off' \
+swayidle -w timeout 2400 'swaylock -f -c  FFC300 ' \
+            timeout 3000 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
-            before-sleep 'swaylock -f -c 000000' &
+            before-sleep 'swaylock -f -c FFC998' &
